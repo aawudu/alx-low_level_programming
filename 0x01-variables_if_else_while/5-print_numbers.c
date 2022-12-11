7914ed18aaebdca51270b4405860c02e0942417c
Aisha Awudu <awuduaisha@gmail.com>
2022-12-11 06:37:02 -0800

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints from 0-9'
+ *
+ * Return: Always 0 success
+ */
+int man(void)
+{
+	printf("0123456789\n");
+	return (0);
+}

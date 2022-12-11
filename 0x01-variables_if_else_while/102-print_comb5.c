027ce2e695a4b7dca105f0bf523d239cc81947f3
Aisha Awudu <awuduaisha@gmail.com>
2022-12-11 08:55:11 -0800

    an update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,11 +16,11 @@ int main(void)
 		{
 			if (m < n && m != n)
 			{
-				putchar((m / 10) + '0');
-				putchar((n % 10) + '0');
-				putchar(' ');
 				putchar((m % 10) + '0');
 				putchar((n / 10) + '0');
+				putchar(' ');
+				putchar((m / 10) + '0');
+				putchar((n % 10) + '0');
 				if (m != 98 || n != 99)
 				{
 					putchar(',');

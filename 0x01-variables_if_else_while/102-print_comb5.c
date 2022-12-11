fb7b2925989161ca0b010e79354dfe6cf196cf8a
Aisha Awudu <awuduaisha@gmail.com>
2022-12-11 08:08:07 -0800

    print possible two pair combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description:'prints possible two digit number combinations'
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int m, n;
+
+	for (m = 0; n <= 99; m++)
+	{
+		for(n = 0; n <= 99; n++;)
+		{
+			if (m < n && m != n)
+			{
+				putchar((m / 10) + '0');
+				putchar((n % 10) + '0');
+				putchar(' ');
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+				if (m != 98 || n != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+

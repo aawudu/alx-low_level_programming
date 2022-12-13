1707900d5648742720052d5f7ddaab9068e02575
Aisha Awudu <awuduaisha@gmail.com>
2022-12-13 13:43:07 -0800

    another commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: always 0 success
+ */
+void print_alphabet(void)
+{
+	int p;
+
+	for (p = 'a'; p <= 'z'; p++)
+	{
+		_putchar(p);
+	}
+	_putchar('\n');
+}
+

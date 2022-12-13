02cd025f227ab277c0c5683a130e4aa27327e74a
Aisha Awudu <awuduaisha@gmail.com>
2022-12-13 15:23:07 -0800

    add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds inputs and returns results
+ * @one: first input
+ * @two: second input
+ * Return: one + two
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}

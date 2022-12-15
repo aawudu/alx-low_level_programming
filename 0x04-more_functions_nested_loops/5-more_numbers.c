d033e3b5adb27477db5a13532ff4dcd1bf0391b3
Aisha Awudu <awuduaisha@gmail.com>
2022-12-15 04:22:19 -0800

    10x numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 by the numbers from 0 to 9
+ * Return: 10 x number from 0 to 9
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 0)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

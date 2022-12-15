287bdad6aecbb6eff67a0fffca376d7d8030f376
Aisha Awudu <awuduaisha@gmail.com>
2022-12-15 04:02:49 -0800

    prodict of numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - product of two numbers
+ * @x: first number input
+ * @y: second number input
+ * Return: result of product of numbers
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}

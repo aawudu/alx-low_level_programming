be9498de5a54ea768f6e33c3d89b0b9811ac17ce
Aisha Awudu <awuduaisha@gmail.com>
2022-12-15 01:33:55 -0800

    print positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - function for testing positive and negative
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}

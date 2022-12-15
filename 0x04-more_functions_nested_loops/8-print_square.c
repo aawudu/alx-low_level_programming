969b0b7a537fa26b4622aadbc4f969dadfc60730
Aisha Awudu <awuduaisha@gmail.com>
2022-12-15 04:42:31 -0800

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - draw number of squares per parameter
+ * @size: number of squares by number of times
+ * Return: empty
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}

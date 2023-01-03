e9ca49355659c97db4abc84a51191b59f0ce51ac
Aisha Awudu <32910090+aawudu@users.noreply.github.com>
2023-01-03 23:24:01 +0000

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,12 +6,12 @@
  */
 void print_chessboard(char(*a)[8])
 {
-	int b;
-	int c;
-	for (b = 0; b < 8; b++)
+	int bee;
+	int cee;
+	for (bee = 0; bee < 8; bee++)
 	{
-		for (c = 0; c < 8; c++)
-			_putchar(a[b][c]);
+		for (cee = 0; cee < 8; cee++)
+			_putchar(a[bee][cee]);
 		_putchar('\n');
 	}
 }

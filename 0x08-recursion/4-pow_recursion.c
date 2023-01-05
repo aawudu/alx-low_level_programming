59f7d06f9eb0dcc1c0df2e7da1a10c3b3108de19
Aisha Awudu <awuduaisha@gmail.com>
2023-01-05 15:26:00 -0800

    added 4-pow_recursion.c file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - print power of ta number
+ * @x: base number
+ * @y: exponential
+ * Return: the power of a number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}

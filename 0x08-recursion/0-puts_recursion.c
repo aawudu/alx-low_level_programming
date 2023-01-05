fdabca150bb061b4e1b4fb1125665a2d9869ddb5
Aisha Awudu <awuduaisha@gmail.com>
2023-01-05 14:15:24 -0800

    updated main.h

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (s[0] != '\0')
 	{
-		return;
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }

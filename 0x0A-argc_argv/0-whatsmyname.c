3c3a53d82e319ad06d94b4da721dd937d7ab6015
Aisha Awudu <awuduaisha@gmail.com>
2023-01-09 09:57:04 -0800

    added main.h, README.md, and 0-whatsmyname.c files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a function that prints its name, followed by a new line.
+ * @argc: counts of argument
+ * @argv: arguement vector
+ * Return: prints name of programme.
+ */
+int main(int argc, char **argv)
+{
+	int koko = 0;
+
+	for (; koko < argc; koko++)
+	{
+		printf("argv[%d] = %s\n", koko, argv[koko]);
+	}
+	return (0);
+}
